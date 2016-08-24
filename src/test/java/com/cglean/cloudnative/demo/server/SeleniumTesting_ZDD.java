f930273bf4e9d27daaacfbbe92d6b61cd9ad0e37
balajiveerappan <balaji.a.veerappan@capgemini.com>
2016-08-24 18:30:53 +0530

    chrome

--- src/test/java/com/cglean/cloudnative/demo/server/SeleniumTesting_ZDD.java
+++ src/test/java/com/cglean/cloudnative/demo/server/SeleniumTesting_ZDD.java
@@ -15,7 +15,7 @@ public class SeleniumTesting_ZDD {
 	public  void SeleniumTesting() throws InterruptedException {
 			
 			{
-				System.setProperty("webdriver.chrome.driver", "\var\lib\jenkins\workspace\SeleniumTest\chromedriver");
+				System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/SeleniumTest/chromedriver");
 				WebDriver driver = new ChromeDriver();
 				//WebDriver driver = new FirefoxDriver();
 				driver.navigate().to("http://shows-cgleanv1.cglean.com/");
